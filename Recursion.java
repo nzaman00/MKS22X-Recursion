07580b0c40f903940e698f5639d95c2fb8334bf8
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-01-31 18:53:06 -0500

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
 public class Recursion{
-
+  public static double sqrt(double n, double tolerance){
+    return sqrter(n, tolerance, 1);
+  }
+  public static double sqrter(double n, double tolerance, double guess){
+   if (Math.abs(((guess * guess - n) / n)) * 100 <= tolerance) 
+  }
 }
